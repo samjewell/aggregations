8572e956cb573f5ca6f120400b76f4ab841d2d95
Sam Jewell <sam@bridge-u.com>
2018-11-24 12:51:22 +0000

    Add solutions to each of the exercises

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Person < ActiveRecord::Base
   belongs_to :location
   belongs_to :role
@@ -5,10 +7,62 @@ class Person < ActiveRecord::Base
   has_many :employees, class_name: "Person", foreign_key: :manager_id
 
   def self.maximum_salary_by_location
-    {}
+    group(:location_id).maximum(:salary)
+  end
+
+  def self.maximum_salary_by_location_name
+    joins(:location).group("locations.name").maximum(:salary)
+  end
+
+  def self.average_salary_by_manager_name
+    joins(:manager).group("managers_people.name").average(:salary)
+  end
+
+  def self.with_lower_than_average_salary_for_their_manager
+    joins(
+      "INNER JOIN (" +
+        Person.
+          select("manager_id, AVG(salary) as average").
+          group("manager_id").
+          to_sql +
+      ") salaries " \
+      "ON salaries.manager_id = people.manager_id"
+    ).
+    where("people.salary < salaries.average")
+  end
+
+  def self.average_salary_of_managers_direct_reports
+    joins(:employees).group(:name).average("employees_people.salary")
   end
 
   def self.managers_by_average_salary_difference
-    all
+    #orders managers by the difference between their salary and the average salary of their employees
+    # joins(
+    #   "INNER JOIN (" +
+    #     Person.
+    #       select("people.id as manager_id, AVG(employees_people.salary) as average").
+    #       joins(:employees).
+    #       group(:id).
+    #       to_sql +
+    #   ") team_average_salaries " \
+    #   "ON team_average_salaries.manager_id = people.id"
+    # ).
+    # order(Arel.sql("salary - team_average_salaries.average DESC"))
+
+    joins(
+      "INNER JOIN (" +
+        Person.
+          select("people.id as manager_id, people.salary - AVG(employees_people.salary) as difference").
+          joins(:employees).
+          group(:id).
+          to_sql +
+      ") salaries " \
+      "ON salaries.manager_id = people.id"
+    ).
+    order("salaries.difference DESC")
+  end
+
+  def to_s
+    "name: #{name}, salary: #{salary}, location_id: #{location_id}"
   end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,10 +1,9 @@
 require "spec_helper"
+require "byebug"
 
 describe Person do
   describe ".maximum_salary_by_location" do
     it "finds the highest salary at each location" do
-      pending "Implement maximum_salary_by_location to make this spec pass"
-
       [50_000, 60_000].each do |highest_salary|
         location = create(:location, name: "highest-#{highest_salary}")
         create(:person, location: location, salary: highest_salary - 1)
@@ -27,10 +26,78 @@ describe Person do
     end
   end
 
+  describe ".maximum_salary_by_location_name" do
+    it "finds the highest salary at each location, returning a hash with location.name keys" do
+      [50_000, 60_000].each do |highest_salary|
+        location = create(:location, name: "highest-#{highest_salary}")
+        create(:person, location: location, salary: highest_salary - 1)
+        create(:person, location: location, salary: highest_salary)
+      end
+
+      result = Person.maximum_salary_by_location_name
+
+      expect(result).to eq(
+        "highest-50000" => 50_000,
+        "highest-60000" => 60_000
+      )
+    end
+  end
+
+  describe ".average_salary_by_manager_name" do
+    it "finds the average salary for each manager, returning a hash with manager.name keys" do
+      [50_000, 60_000].each do |mean_salary|
+        manager = create(:person, name: "mean-approx-#{mean_salary}")
+        create(:person, manager: manager, salary: mean_salary - 5)
+        create(:person, manager: manager, salary: mean_salary - 1)
+        create(:person, manager: manager, salary: mean_salary)
+      end
+
+      result = Person.average_salary_by_manager_name
+
+      expect(result).to eq(
+        "mean-approx-50000" => 49_998,
+        "mean-approx-60000" => 59_998
+      )
+    end
+  end
+
+  describe ".with_lower_than_average_salary_for_their_manager" do
+    it "finds employees who have a salary lower than the average for their manager's direct reports" do
+      [50_000, 60_000].each do |mean_salary|
+        manager = create(:person, name: "mean-approx-#{mean_salary}")
+        create(:person, manager: manager, salary: mean_salary - 5, name: "below-average-for-manager-with-mean-salary-#{mean_salary}")
+        create(:person, manager: manager, salary: mean_salary - 1)
+        create(:person, manager: manager, salary: mean_salary)
+      end
+
+      result = Person.with_lower_than_average_salary_for_their_manager
+
+      expect(result.map(&:name)).to eq(%w(
+        below-average-for-manager-with-mean-salary-50000
+        below-average-for-manager-with-mean-salary-60000
+      ))
+    end
+  end
+
+  describe ".average_salary_of_managers_direct_reports" do
+    it "finds the average salary for each manager, returning a hash with manager.name keys" do
+      [50_000, 60_000].each do |mean_salary|
+        manager = create(:person, name: "mean-approx-#{mean_salary}")
+        create(:person, manager: manager, salary: mean_salary - 2)
+        create(:person, manager: manager, salary: mean_salary)
+      end
+
+      result = Person.average_salary_of_managers_direct_reports
+
+      expect(result).to eq(
+        "mean-approx-50000" => 49_999,
+        "mean-approx-60000" => 59_999
+      )
+    end
+  end
+
   describe ".managers_by_average_salary_difference" do
     it "orders managers by the difference between their salary and the average salary of their employees" do
-      pending "Implement managers_by_average_salary_difference to make this spec pass"
-
       highest_difference = [45_000, 20_000]
       medium_difference = [50_000, 10_000]
       lowest_difference = [50_000, -5_000]
